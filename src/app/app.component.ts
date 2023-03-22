a52b8ed6b288a5eef1b02cd6a60a87813658d5e1
gabrieleorro <53332032+gabrieleorro@users.noreply.github.com>
2023-03-22 18:47:23 +0100

    Inizio web app ZMOG, implementata navbar e carousel

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { faAddressCard, faShirt } from '@fortawesome/free-solid-svg-icons';
+import { faReceipt } from '@fortawesome/free-solid-svg-icons';
+import { faHotel } from '@fortawesome/free-solid-svg-icons';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'zmog';
+
+  faAddressCard = faAddressCard;
+  faReceipt = faReceipt;
+  faHotel = faHotel;
+  faShirt = faShirt;
+  faCartArrowDown = faCartArrowDown;
+
+  images = [
+    {
+      id: 1,
+      label: 'Prodotto 1'
+    },
+    {
+      id: 2,
+      label: 'Prodotto 2'
+    },
+  ];
+
+  percorso = "../assets/images/carousel-";
+}
